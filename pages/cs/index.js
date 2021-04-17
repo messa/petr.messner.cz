090b90f4fa8e1e72aaa3b6bec7f0bdca3049bae4
Petr Messner <petr.messner@gmail.com>
2021-04-17 22:32:08 +0200

    add empty index pages

---
+++ pages/cs/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function IndexPage() {
+  return (
+    <div>
+      <h1>Index page CS</h1>
+    </div>
+  )
+}
+
+export default IndexPage
---
+++ pages/en/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function IndexPage() {
+  return (
+    <div>
+      <h1>Index page EN</h1>
+    </div>
+  )
+}
+
+export default IndexPage
