06437f4e4a99f826e996f63cbc33ecab0477f0b5
Petr Messner <petr.messner@gmail.com>
2021-04-18 17:44:51 +0200

    add client-side redirect to /cs/ or /en/ in top-level index page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 function IndexPage() {
+  const router = useRouter()
+  const [ showMenu, setShowMenu ] = useState(false)
+  useEffect(() => {
+    if (/^cs/.test(navigator.language)) {
+      router.push('/cs/')
+    } else {
+      router.push('/en/')
+    }
+  })
   return (
     <div>
-      <p style={{ textAlign: 'center', fontSize: '200%', padding: '40px' }}>
-        <Link href='/cs/'><a>česky</a></Link>
-        {' · '}
-        <Link href='/en/'><a>english</a></Link>
-      </p>
+      {showMenu && (
+        <p style={{ textAlign: 'center', fontSize: '200%', padding: '40px' }}>
+          <Link href='/cs/'><a>česky</a></Link>
+          {' · '}
+          <Link href='/en/'><a>english</a></Link>
+        </p>
+      )}
     </div>
   )
 }
